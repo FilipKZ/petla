e8a1f021db2ede079949b16ff671ff974501bccb
Filip Karcz <filip.karcz@gmail.com>
2019-03-02 11:15:51 +0100

    zmiana

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
+        Scanner sc = new Scanner(System.in);
+        int i = Integer.parseInt(sc.nextLine());
+
+        for (i = 0; i < 10; i++) {
             System.out.println(i);
         }
     }
